cfdeb06c4dc8346351caa02c00c6eb0a13c66288
Joe McGill <joemcgill@gmail.com>
2021-07-22 09:16:34 -0500

    Lesson 1 w/ extra credit
    This adds a useState hook for managing the value for `name` and adds a initialName property to the component that can optionally be passed as the default name value.

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,28 +2,38 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
+import PropTypes from 'prop-types';
 
-function Greeting() {
+function Greeting({ initialName }) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value);
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name}/>
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
   )
 }
 
+Greeting.defaultProps = {
+  initialName: ''
+};
+
+Greeting.propTypes = {
+  initialName: PropTypes.string,
+};
+
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Joe" />
 }
 
 export default App
